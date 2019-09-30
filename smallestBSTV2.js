cbe30954d2f2890a23f29f49514a06ef3c00c1f4
aceos2201 <tue79528@temple.edu>
2019-09-29 21:48:36 -0400

    Fixed oversight where the entire tree was traversed before returning the kth value

--- smallestBSTV2.js
+++ smallestBSTV2.js
@@ -1,10 +1,7 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+/** Sept 29, 2019 - faster than 94.11% - less memory than 72.73%
+*  ---> Completion Time: NA <---
+*  
+*/
 /**
  * @param {number[]} root
  * @param {number} k
@@ -24,48 +21,50 @@ var kthSmallest = function(root, k) {
      var stack = [];
      var Current = root;
      var result;
+     var n = 0;
  
      while(Current != null) {
 
          if(Current.left == null) {
-             stack.push(Current.val);
-             Current = Current.right;
-             if(stack.length == k){ 
-                result = stack.pop();
-                Current = null;
-             }
+            stack.push(Current.val);
+            n++;
+            if(stack.length == k){ 
+                return stack.pop();
+            }
+            // Move on to RIGHT subtree --> iterate next loop
+            Current = Current.right;
          }else {
             // Move to the left child
             let node = Current.left;
             // Keep taking the right branch until you find NULL or the "Current" node
             while(node.right != null && node.right != Current) {
-                        node = node.right;
+                node = node.right;
             }
-            // Right child is NULL 
+            // If Right child is NULL 
             if(node.right == null) {
                 // point back to the "Predecessor"
                 node.right = Current;
                 // Move to the LEFT subtree --> iterate next loop
                 Current = Current.left;
-            // We have looped back to a "Predecessor"
+            // If we have looped back to a "Predecessor"
             }else if(node.right == Current) {
                 // Remove pointer
                 node.right = null;
+                n++;
                 stack.push(Current.val); // For logging purposes only
+                if(stack.length == k){ 
+                    return stack.pop();
+                }
                 // Move on to RIGHT subtree --> iterate next loop
                 Current = Current.right;
-
-                if(stack.length == k){ 
-                    result = stack.pop();
-                    Current = null;
-                }
-             }
+            }
          }
      }
  
-     while(stack.length > 0) {
+     /*while(stack.length > 0) {
          console.log(stack.pop());
      }
+     */
      return result;
  };
 
